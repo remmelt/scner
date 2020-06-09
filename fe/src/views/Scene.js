d5aadcd0d68bf7cc90da1c34703352bea0962906
Remmelt Pit <remmelt@hipcode.io>
2020-06-09 16:14:38 +0200

    autoWIP

--- fe/src/views/Scene.js
+++ fe/src/views/Scene.js
@@ -6,13 +6,17 @@ module.exports = {
   oninit: function(vnode) {
     Scene.load(vnode.attrs.id);
   },
-
   view: function() {
-    scene = Scene.current;
+    scene = Scene.current
 
     return [
       m('div.scene', [
-        m('.persons', Scene.current.name)
+        m('span', scene.name),
+        m('img', {src: scene.image == null ? app.placeholder : 'media/' + scene.image, width: '100px'}),
+        m('img', {src: scene.thumbs == null ? app.placeholder : 'media/' + scene.thumbs, width: '100px'}),
+        m('ul', scene.persons.map(function(person) {
+            return m('.person', person.name)
+        }))
       ]),
     ];
   },
